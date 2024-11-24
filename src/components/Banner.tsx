a88a0958d3085e031fbd873f4b7931db7105ca62
JanselLopez <21jansel@gmail.com>
2024-11-24 15:12:32 -0500

    test-react-seo

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <a href='/remesas'>
+            Comprar remesas para Cuba por Paypal
+        </a>
+    </div>
+  )
+}
+
+export default Banner
